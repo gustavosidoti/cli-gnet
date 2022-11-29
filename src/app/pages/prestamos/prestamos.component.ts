462bd604697fcc13aa92a3b2493e269a4d968b2a
gustavosidoti <gustavosidoti@gmail.com>
2022-11-28 23:43:29 -0300

    11-28

--- src/app/pages/prestamos/prestamos.component.ts
+++ src/app/pages/prestamos/prestamos.component.ts
@@ -11,17 +11,35 @@ export class PrestamosComponent implements OnInit {
 
   public totalPrestamos: number = 0;
   public prestamos: Prestamos[] = [];
+  public desde: number = 0;
 
   constructor(private prestamosService: PrestamosService) { }
 
   ngOnInit(): void {
-    this.prestamosService.cargarPrestamos(0)
+    
+     this.cargarPrestamos();
+    
+  }
+
+  cargarPrestamos () {
+    this.prestamosService.cargarPrestamos( this.desde )
          .subscribe( ({total, prestamos}) => {
           this.totalPrestamos = total;
           this.prestamos = prestamos;
+          
+          
          })
+  }
 
-    
+  cambiarPagina( valor: number){
+    this.desde += valor;
+
+    if ( this.desde < 0) {
+      this.desde = 0;
+    } else if ( this.desde >= this.totalPrestamos ) {
+      this.desde -= valor;
+    }
+      this.cargarPrestamos();
   }
 
   
